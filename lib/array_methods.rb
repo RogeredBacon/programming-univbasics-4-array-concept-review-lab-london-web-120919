9236021282d6be5f5cedd11fdd9cc295229dc3b3
James Inglis <jinglis12@googlemail.com>
2019-11-27 12:52:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times { |index| 
-    if(array[index] == value_to_find) {
+    if(array[index] == value_to_find)
       return index
-    }
+    end
   }
-  return nil
 end
 
 def find_max_value(array)
