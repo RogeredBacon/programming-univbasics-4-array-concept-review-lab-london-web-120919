f5dadc385e48dce836de64d80749db5497fec4f7
James Inglis <jinglis12@googlemail.com>
2019-11-27 13:00:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,7 @@ end
 
 def find_max_value(array)
   # array.sort[-1]
-  num = 0
+  num = array[0]
   array.length.times { |index| 
     if(array[index] > num)
       num = array[index]
@@ -20,7 +20,7 @@ end
 
 def find_min_value(array)
   # array.sort[0]
-  num = 0
+  num = array[0]
   array.length.times { |index| 
     if(array[index] < num)
       num = array[index]
